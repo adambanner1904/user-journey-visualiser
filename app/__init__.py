0ad657768e2f2964e2eaa6493403e7e1117a39e7
adambanner <adamajbbanner@hotmail.co.uk>
2025-05-13 12:15:41 +0100

    Refactored one blueprint into two different controllers one for the managing dataset side of it and one for the graph visualisation.

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template # type: ignore
 
 def create_app():
     app = Flask(__name__)
-
-    from .controller import bp
-    app.register_blueprint(bp)
+    app.config['SECRET_KEY'] = b'sentearntnqu19304'
+    from .controllers.ujv import ujv
+    from .controllers.projects import projects
+    app.register_blueprint(ujv)
+    app.register_blueprint(projects)
 
     return app
     
