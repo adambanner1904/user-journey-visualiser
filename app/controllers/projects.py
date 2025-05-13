20c58237e95967e6c9d005f8ca1fc4638b8d7c54
adambanner <adamajbbanner@hotmail.co.uk>
2025-05-13 16:42:30 +0100

    Work on main projects.py logic

--- app/controllers/projects.py
+++ app/controllers/projects.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from flask import Blueprint, render_template, request, flash, redirect, url_for  # type: ignore
 import pandas as pd
 import os
@@ -8,18 +10,15 @@ projects = Blueprint('projects', __name__)
 
 @projects.route('/projects', methods=["GET", "POST"])
 def manage_projects():
-    flash("This works")
     if request.method == "POST":
         name = request.form['name'].lower().replace(' ', '-')
-        project_dir = os.path.join("/user-journey-visualiser/app/static/projects", name)
-        if os.path.exists(project_dir):
-            flash("This project already exists", 'error')
-            return redirect(url_for('manage_projects'))
+        file = request.files['data']
+        csv = pd.read_csv(file)
+        print(csv)
 
-        os.mkdir(project_dir)
-        print(f"{project_dir} directory created!")
-        # file = request.files['data']
-        # print(file)
-
-
+        with sqlite3.connect("temp.db") as conn:
+            cur = conn.cursor()
+            results = cur.execute("select * from temp")
+            for result in results:
+                print(result[0])
     return render_template('projects.html')
