61b347970e505e944200383bcccca80649aaed33
adambanner <adamajbbanner@hotmail.co.uk>
2025-05-13 16:42:14 +0100

    Wire framed db functions

---
+++ app/db/__init__.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Insert data from pandas dataframe into table
+def insert_dataframe_into_table(df: pd.DataFrame) -> None:
+    return
+
+# Should create a new table with the name of the project
+# Name will be used later for dropdown values
+def create_table(name:str) -> None:
+    return
+
+# Should return whether that name already exists as a table
+def table_exists(name: str) -> bool:
+
+    return False
