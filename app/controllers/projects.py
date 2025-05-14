50f79b64dd87c7014dc2b6bdb5042c1420c35d72
adambanner <adamajbbanner@hotmail.co.uk>
2025-05-14 16:05:59 +0100

    Implemented add-project functionality and split it from the main manage_projects view so that I can make room for delete-project

--- app/controllers/projects.py
+++ app/controllers/projects.py
@@ -3,44 +3,45 @@ import string
 
 from flask import Blueprint, render_template, request, flash, redirect, url_for  # type: ignore
 import pandas as pd
-import os
 
-from app.db import create_table, table_exists
+from app.db import create_table, table_exists, insert_dataframe_into_table
 
-projects = Blueprint('projects', __name__)
+projects = Blueprint('projects', __name__, url_prefix='/projects')
 
 
-@projects.route('/projects', methods=["GET", "POST"])
+@projects.route('', methods=["GET"])
 def manage_projects():
-    if request.method == "POST":
-        name = request.form['name']
-        file = request.files['data']
-
-        # Name checks
-        if not is_valid_name(name):
-            flash("Name does not match the specified format", category='error')
-            return redirect(request.url)
-        name = format_name(name)
-
-        # Check if formatted name is already a table name
-        # Cant have duplicate tables
-        if table_exists(name):
-            flash(f"There already exists a project with the name {name}", category="error")
-            return redirect(request.url)
-
-        # File checks
-        if file.filename == '':
-            flash("File is required!", category='error')
-            return redirect(request.url)
-
-        csv = pd.read_csv(file)
-        create_table(name)
-        # with sqlite3.connect("temp.db") as conn:
-        #     cur = conn.cursor()
-        #     results = cur.execute("select * from temp")
-        #     for result in results:
-        #         print(result[0])
     return render_template('projects.html')
+
+
+@projects.route('/add-project', methods=['POST'])
+def add_project():
+    name = request.form['name']
+    file = request.files['data']
+
+    # Name checks
+    if not is_valid_name(name):
+        flash("Name does not match the specified format", category='error')
+        return redirect(request.url)
+
+    formatted_name = format_name(name)
+
+    # Check if formatted name is already a table name
+    # Cant have duplicate tables
+    if table_exists(formatted_name):
+        flash(f"There already exists a project with the name {name}", category="error")
+        return redirect(request.url)
+
+    # File checks
+    if file.filename == '':
+        flash("File is required!", category='error')
+        return redirect(request.url)
+
+    data = pd.read_csv(file)
+
+    create_table(formatted_name)
+    insert_dataframe_into_table(formatted_name, data)
+    return redirect(url_for('projects.manage_projects'))
 
 
 def format_name(name: str):
@@ -48,6 +49,7 @@ def format_name(name: str):
         return redirect(request.url)
     print(f'My name is {name}')
     return name.replace('-', '_').replace(' ', '_')
+
 
 def is_valid_name(name: str) -> bool:
     allowed_chars = set(string.ascii_letters + '-_ ')
--- app/db/__init__.py
+++ app/db/__init__.py
@@ -2,23 +2,37 @@ import pandas as pd
 import sqlite3
 
 DATABASE_PATH = 'temp.db'
+SCHEMA = "(previous_page TEXT, page TEXT, page_views INT, unique_page_views INT)"
+
+def create_connection():
+    con = sqlite3.connect(DATABASE_PATH)
+    con.row_factory = sqlite3.Row
+    return con
+
 
 # Insert data from pandas dataframe into table
-def insert_dataframe_into_table(df: pd.DataFrame) -> None:
-    return
+def insert_dataframe_into_table(table_name: str, df: pd.DataFrame) -> None:
+    # Convert df to list of tuples
+    records = list(df.itertuples(index=False, name=None))
+
+    with create_connection() as con:
+        con.executemany(f'INSERT INTO "{table_name}" VALUES(?, ?, ?, ?);', records)
+
 
 # Should create a new table with the name of the project
 # Name will be used later for dropdown values
-def create_table(name:str) -> None:
+def create_table(name: str) -> None:
+    with create_connection() as con:
+        con.execute(f'CREATE TABLE {name} {SCHEMA}')
     return
+
 
 # Should return whether that name already exists as a table
 def table_exists(name: str) -> bool:
-    with sqlite3.connect(DATABASE_PATH) as con:
-        cur = con.cursor()
-
+    with create_connection() as con:
         # Flattened list of table names list
-        results: list[str] = [result[0] for result in cur.execute("select name from sqlite_master where type= 'table';").fetchall()]
+        results: list[str] = [result[0] for result in
+                              con.execute("select name from sqlite_master where type= 'table';").fetchall()]
         if name in results:
             return True
 
